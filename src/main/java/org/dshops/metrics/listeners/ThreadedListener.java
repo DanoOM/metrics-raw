a7ad25f7b8aff8db5cc99dd0772f90e695728d4b
dhopkins <dhopkins@dhops-devbox>
2017-02-10 15:19:35 -0800

    Release Prep

--- src/main/java/org/dshops/metrics/listeners/ThreadedListener.java
+++ src/main/java/org/dshops/metrics/listeners/ThreadedListener.java
@@ -8,13 +8,14 @@ public abstract class ThreadedListener implements EventListener {
 
     @Override
     public void stop() {
-        if(runThread != null && runThread.isAlive()) {
+        if (runThread != null && runThread.isAlive()) {
             stopRequested = true;
             long startWait = System.currentTimeMillis();
             try {
                 do {
                     Thread.sleep(100);
                 } while(runThread.isAlive() && System.currentTimeMillis() - startWait < 2000);
+
                 if(runThread.isAlive()) {
                     runThread.interrupt();
                 }
